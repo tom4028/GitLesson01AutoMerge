a7023474c3e5752a704eb65870ff8a4835023b5f
Adam Zaręba <adam.d.zareba@gmail.com>
2019-03-05 15:46:28 +0100

    Added supper to meals

--- src/meals.js
+++ src/meals.js
@@ -1,5 +1,5 @@
 var meals = ['breakfast', 'lunch', 'dinner'];
-
+meals.push('supper');
 console.log(meals.sort());
 meals.forEach(function(currentValue, index, arr){
   console.log(index, currentValue, arr);
