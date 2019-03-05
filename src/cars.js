3dafe6c75a7c52f3fe608310a2c9ecfe99b30d96
Adam Zaręba <adam.d.zareba@gmail.com>
2019-03-05 15:14:40 +0100

    Removed cars, added tv series

--- src/cars.js
+++
@@ -1,8 +0,0 @@
-var cars = ['BMW', 'Audi', 'Volvo'];
-
-console.log(cars.sort());
-cars.forEach(function(currentValue, index, arr){
-  console.log(index, currentValue, arr);
-});
-
-console.log(cars[1]);
---
+++ src/tvSeries.js
@@ -0,0 +1,11 @@
+var tvSeries = ['Game of Thrones', 'Stranger Things', 'Black Mirror'];
+
+console.log(tvSeries.sort());
+tvSeries.forEach(function(currentValue, index, arr){
+  console.log(index, currentValue, arr);
+});
+
+console.log(tvSeries[1]);
+console.log(tvSeries[tvSeries.length - 1]);
+
+var tvSeriesCopy = tvSeries.slice();
